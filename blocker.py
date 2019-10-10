970ab3da1cd6b3bb21bbe66fea865e121242466c
Matthew Deakos <mwoolnerdeakos@gmail.com>
2019-10-10 01:03:13 -0300

    Fixed some bugs in writing to hostfile

--- blocker.py
+++ blocker.py
@@ -4,20 +4,45 @@ import time
 from time import localtime
 import datetime
 from dateutil.relativedelta import relativedelta
-
 import logging
 from utils import within_1d, convert_to_s
 
+def hostfile_contains(target_string):
+    with open('/etc/hosts', 'r') as hostfile:
+        lines = hostfile.readlines()
+        for line in lines:
+            if target_string in line:
+                return True
+
+    return False
+
 def allow(website):
-    with open('/etc/hosts', 'w') as hostfile:
+    target_string = f'127.0.0.1 {website}'
+
+    if not hostfile_contains(target_string):
+        logging.warning(f"Allow action scheduled for currently permitted website: {website}. Skipping")
+        return
+
+    with open('/etc/hosts', 'r+') as hostfile:
         logging.debug(f"Allowing {website}")
         # TODO: delete line in hostfile
-        pass
+        lines = hostfile.readlines()
+        hostfile.seek(0)
+        for line in lines:
+
+            if target_string not in line:
+                hostfile.write(line)
+        hostfile.truncate()
 
 def block(website):
-    with open('/etc/hosts', 'w+') as hostfile:
+    target_string = f'127.0.0.1 {website}\n'
+    if hostfile_contains(target_string):
+        logging.warning(f"Block action scheduled for currently blocked website: {website}. Skipping.")
+        return
+
+    with open('/etc/hosts', 'a+') as hostfile:
         logging.debug(f"Blocking {website}")
-        hostfile.write(f'127.0.0.1 {website}')
+        hostfile.write(target_string)
 
 
 def scheduler(schedule_list):
@@ -50,13 +75,13 @@ def run(config_args):
         sleep_time_s = convert_to_s(sleep_time)
 
         # Pause until next scheduled item
-        logging.info(f"{action},{allow} scheduled at {next_execution}")
+        logging.info(f"{action},{website} scheduled at {next_execution}")
         logging.debug(f"Sleeping for {sleep_time_s} seconds")
         time.sleep(sleep_time_s)
 
-        if action == 'allow':
+        if action == 'ALLOW':
             allow(website)
-        elif action == 'block':
+        elif action == 'BLOCK':
             block(website)
         else: # Reset
             schedule = scheduler(config_args)
